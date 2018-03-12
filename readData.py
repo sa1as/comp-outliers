4cf477f2759d4f8ce2331b8a81b8001a9c5da8f9
sa1as <sa1as@github.com>
2018-03-12 22:08:30 +0200

    read data

--- readData.py
+++ readData.py
@@ -9,7 +9,7 @@ This is a temporary script file.
 # python packages are available on the system.
 
 import matplotlib.pyplot as plt
-import scipy
+import scipy.io.wavfile
 import numpy as np
 import wave
 import sys
@@ -17,28 +17,33 @@ import glob
 import pandas as pd
 
 
-data2 = scipy.io.wavfile.read('/home/doltsinis/Documents/Composition/acoustic data/acoustic 1/180108151705.wav')
+data2 = scipy.io.wavfile.read('/home/saias/Documents/composition/acoustic data/acoustic 1/180108151705.wav')
 data = wave.read('/home/doltsinis/Documents/Composition/acoustic data/acoustic 1/180108151705.wav')
-
 plt.plot(data2[1])
 
-print(glob.glob('/home/doltsinis/Documents/Composition/acoustic data/acoustic 1/*.wav'))
-fileNames = sorted(glob.glob('/home/doltsinis/Documents/Composition/acoustic data/acoustic 1/*.wav'))
+paths = ('/home/saias/Documents/composition/acoustic data/acoustic 1/*.wav', '/home/doltsinis/Documents/Composition/acoustic data/acoustic 1/*.wav')
+path = paths[0]
+fileNames = sorted(glob.glob(path)) # get the file names from the whole folder in path
+data = np.ones([len(scipy.io.wavfile.read(fileNames[0])[1]), len(glob.glob(path))]) # intialize data matrix based on folder size
 
-data = np.ones(len(scipy.io.wavfile.read(fileNames[1])), len(glob.glob('/home/doltsinis/Documents/Composition/acoustic data/acoustic 1/*.wav')))
 
-for i in range(3):
+for i in range(len(glob.glob(path))) :
     print(i)
-    tempData = (scipy.io.wavfile.read(fileNames[i]))
-    test = tempData[1]
-    data = np.concatenate(data,test)
+    tempData = (scipy.io.wavfile.read(fileNames[i])[1])
+    data[:,i] = tempData
     
+plt.figure(i)
+plt.plot(data[:,100])    
+
+# end of loop
+
+    
+
+    np.concatenate(data,tempData)
+    tempData.shape
+    data.shape    
     data = np.asarray(tempData[i])
     
-    plt.figure
-    plt.plot(data[1])
-    plt.legend
-
 
 
 
