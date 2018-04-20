a2c8bf3a5c09eef0d3fb07acf7f9c6b04993b81b
sa1as <doltsinis@gmail.com>
2018-04-20 17:34:01 +0300

    first update with plots

--- outlierMethods.py
+++ outlierMethods.py
@@ -2,10 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 directory = '/home/doltsinis/net_folders/Monsoon/ALDK Electrolysis Optimization Scenario/'
-sampleData = pd.read_csv(directory + 'aldk_tab_1mn.csv', sep=';', nrows=10000)
+sampleData = pd.read_csv(directory + 'aldk_tab_1mn.csv', sep=';', nrows=1000)
 
 
-def MAD(data, windowSize=100):
+def MAD(data, windowSize=200):
     outlierIndex = []
 
     for col in data.columns:
@@ -18,11 +18,18 @@ def MAD(data, windowSize=100):
                 MAD = data[col].iloc[i: i + windowSize - 1].mad()
                 if (windowsMedian + 3 * MAD < data[col].iloc[i + windowSize]) or (
                         windowsMedian - 3 * MAD > data[col].iloc[i + windowSize]):
-                    outlierIndex.append(i)
-                    #print(windowsMedian - 3 * MAD, data[col].iloc[i + windowSize], windowsMedian + 3 * MAD)
-                    #print('outlier in ',col , ', position : ', i)
+                    outlierIndex.append(i + windowSize)
+
+        plt.figure()
+        plt.plot(sampleData[col])
+        plt.scatter(outlierIndex, sampleData[col].iloc[outlierIndex], c='r')
+        #plt.legend()
+        plt.show()
+
+    print('There where', len(outlierIndex), 'outliers found in', len(data), 'data points')
+
     return outlierIndex
 
 
-outliers = MAD(sampleData.iloc[:, 5:6])
-print(len(outliers))
+outliers = MAD(sampleData.iloc[:, 5:7])
+
